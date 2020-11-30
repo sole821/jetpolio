6f5418b44a7a233c2b40e9193e6bae9ae8fee616
sole821 <56422103+sole821@users.noreply.github.com>
2020-11-30 09:12:29 +0900

    upload

--- js/script_work-list.js
+++ js/script_work-list.js
@@ -3,14 +3,30 @@ console.log("Script Load");
 (function($) {
 /*  */
 
-var arrowBtn = $('.arrow');
+
+$(document).ready(function(){
+    console.log('now ready');
+    $('nav').addClass('animate__animated animate__fadeIn');
+    $('.arrow-b').addClass('animate__animated animate__zoomIn');
+    $('.pic-content').addClass('animate__animated animate__flipInX');
+});
+
+const $detail = $('.work-detail');
+const $Bbtn = $('.back-btn');
+
+var arrowBtn = $('.arrow-b');
 var workBox = $('.item');
+var $picCon = $('.item')[0];
+var $width = $picCon.clientWidth;
+
+console.log($width);
 
 var _cuId = 0,
     _exId = _cuId,
     _max = workBox.length;
 
 var _isAni = false;
+
 
 /* 버튼제어 */
 arrowBtn.on('click',function(e){
@@ -29,23 +45,16 @@ arrowBtn.on('click',function(e){
         _cuId = 0;
         }
     }
-    var left = 1250 * _cuId * -1;
+    var left = $width * _cuId * -1;
     /* var duration = 350 + Math.abs(_exId - _cuId) * 150; */
-    workBox.stop(true).animate({'left' : left}, {duration: 800, complete: function () {
-        /* console.log('complete'); */
-        $('.text-box').css('opcity','1');
-        $('.text-box').addClass('animate__animated animate__fadeInDown');
-        setTimeout(function(){
-            $('.text-box').removeClass('animate__animated animate__fadeInDown');
-            /* workBox.removeClass('animate__animated animate__slideInRight'); */
-        },1000);
-    }});   
+    Content_animation(); 
 });
 
 /* 스크롤 제어 */
 $('body').on('scroll mousewheel', function(event) {	
-		       
-    var direction = event.originalEvent.wheelDelta; //마우스 휠 방향
+	if ($detail.css("display") == "block") return;
+    var direction = event.originalEvent.wheelDelta;
+    //마우스 휠 방향
 
     if( direction > 0){
         // up
@@ -61,31 +70,112 @@ $('body').on('scroll mousewheel', function(event) {
         _cuId = 0;
         }	
     }
-    var left = 1250 * _cuId * -1;
-
-    workBox.stop(true).animate({'left' : left}, {duration: 800, complete: function () {
-        /* console.log('complete'); */
-        // $('.text-box').css('opcity','1');
-        // $('.text-box').addClass('animate__animated animate__fadeInDown');
-        setTimeout(function(){
-            $('.text-box').removeClass('animate__animated animate__fadeInDown');
-            /* workBox.removeClass('animate__animated animate__slideInRight'); */
-        },1000);
-    }});
+  
+    Content_animation(); 
 });
 
-/*  */
-
-var pic = $('.pic-container > div > a > img');
-
-workBox.on('mouseover', smallSize);
-
-function smallSize() {
-    // console.log('enter');
-    //workBox.animate({'width' : '500px'},{duration: 800});
-    // pic.css('width','20%');
+function Content_animation() {
+    var left = $width * _cuId * -1;
+    workBox.stop(true).animate({'left' : left}, {duration: 800, complete: function () {
+        /* console.log('complete'); */
+        $('.text-box').addClass('animate__animated animate__fadeIn');
+        setTimeout(function(){
+            $('.thumbnail').removeClass('animate__animated animate__fadeInUp');
+        },1000);
+    }});  
 }
 
+$(".item").on({ 
+    mouseenter: function() {
+        $(".text-box").addClass('text-color');
+        $('.thumbnail').addClass('animate__animated animate__fadeInLeft');
+        $('.thumbnail').css('opacity','1');
+    },
+    mouseleave: function() {
+        $(".text-box").removeClass('text-color');
+        $('.thumbnail').removeClass('animate__animated animate__fadeInLeft');
+        $('.thumbnail').css('opacity','0');
+    }
+});
+
+$("nav").children('a').on({ 
+    mouseenter: function() {
+        $(this).addClass('text-color');
+    },
+    mouseleave: function() {
+        $(this).removeClass('text-color');
+    }
+});
+
+$(".arrow-b").on({ 
+    mouseenter: function() {
+        $(this).children('img').addClass('animate__animated animate__pulse');
+    },
+    mouseleave: function() {
+        $(this).children('img').removeClass('animate__animated animate__pulse');
+    }
+});
+
+
+
+/*  */
+workBox.on('click', function(){
+    $detail.css('transform','translateY(0%)');
+    $detail.css('display','block');
+    var $number = $(this).attr("id");
+    detail_load($number);
+    setTimeout(detail_ani(),3000);
+});
+
+$Bbtn.on('click',function(){
+    $detail.css('transform','translateY(100%)');
+    $detail.css('display','none');
+});
+
+
+/* detail */
+const content = $('.content');
+
+const title = $('.title');
+const type = $('.type');
+const text = $('.text');
+const tool = $('.tool');
+const date = $('.date');
+const imgBox = $('.img-box');
+const blackLine = $('.black-line');
+
+var num = $('.btn-area > .number');
+
+function detail_load($number) {
+    console.log($number);
+
+    var title_info = Myworks[$number].title;
+    console.log(title_info);
+    title.html(title_info);
+
+    var type_info = Myworks[$number].type;
+    type.html(type_info);
+
+    var text_info = Myworks[$number].text;
+    text.html(text_info);
+
+    var tool_info = Myworks[$number].tool;
+    tool.html(tool_info);
+
+    var date_info = Myworks[$number].date;
+    date.html(date_info);
+    
+    var img_info = Myworks[$number].img;
+    imgBox.html(img_info);
+}
+
+function detail_ani(){
+
+}
+
+num.on('click', function() {
+    $('html,body').animate({scrollTop:0}, 500); 
+});
 
 
 
---
+++ js/works-info.js
@@ -0,0 +1,102 @@
+const nowadays = {
+    title: "Nowadays,",
+    type: 'type: project',
+    text: 'Nowadays,는 개인 맞춤형 문화 큐레이팅 기반 앱 서비스 입니다. </br> 서비스 기획과 앱 서비스 / 웹 페이지 디자인에도 참여하였으며, </br> 웹 페이지와 프로모션 영상 작업에도 참여 하였습니다.',
+    tool: "tool : Visual Studio Code / JavaScript / Adobe  Photoshop / Adobe XD / Adobe After Effects",
+    date: "2020.07.06",
+    img:[
+        '<iframe src="https://www.youtube.com/embed/KxJUlXd6FT4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        '<img src="./img/work_1/1-0.jpg" alt="">',
+        '<img src="./img/work_1/1-1.jpg" alt="">',
+        '<img src="./img/work_1/1-2.jpg" alt="">',
+        '<img src="./img/work_1/1-3.jpg" alt="">',
+        '<img src="./img/work_1/1-4.jpg" alt="">',
+        '<img src="./img/work_1/1-6.jpg" alt="">',
+        '<img src="./img/work_1/1-8.jpg" alt="">'
+        ]
+
+};
+const remain = {
+    title: "Remain, Re-main",
+    type: 'type: branding, website',
+    text: '계원예술대학교의 2020년 졸업 전시회의 <br>     브랜딩 및 웹사이트 제작 프로젝트입니다. <br> 졸업 전시 작품을 전시하는 목적으로 제작 되었으며<br> 아이덴티티인 픽셀을 활용하여 다시 주인공이 되는, 계속 되는 학생들의 여정을 표현했습니다.',
+    tool: "tool : Visual Studio Code / JavaScript / Adobe  Photoshop / Adobe XD / Adobe Illustrator",
+    date: "2020.09.11",
+    img:[
+        '<iframe src="https://www.youtube.com/embed/KxJUlXd6FT4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        '<img src="./img/work_1/1-0.jpg" alt="">',
+        '<img src="./img/work_1/1-1.jpg" alt="">',
+        '<img src="./img/work_1/1-2.jpg" alt="">',
+        '<img src="./img/work_1/1-3.jpg" alt="">',
+        '<img src="./img/work_1/1-4.jpg" alt="">',
+        '<img src="./img/work_1/1-6.jpg" alt="">',
+        '<img src="./img/work_1/1-8.jpg" alt="">'
+        ]
+
+};
+const werk = {
+    title: "werk",
+    type: 'type: web site',
+    text: '카페 브랜드 Werk의 웹페이지 리뉴얼 작업입니다. <br> 브랜드의 특성을 고려해 모던하면서도 감각적인 느낌의 웹페이지 디자인을 진행하였고, <br> JavaScript를 통한 인터렉션이 포함 된 웹 구현 작업을 진행하였습니다.',
+    tool: "tool : Visual Studio Code / Adobe  Photoshop / JavaScript",
+    date: "2020.07.09",
+    img:[
+        '<img src="./img/work_2/2-0.jpg" alt="">',
+        '<img src="./img/work_2/2-1.jpg" alt="">',
+        '<img src="./img/work_2/2-2.jpg" alt="">',
+        '<img src="./img/work_2/2-3.jpg" alt="">',
+        '<img src="./img/work_2/2-4.jpg" alt="">',
+        '<img src="./img/work_2/2-5.jpg" alt="">',
+        '<img src="./img/work_2/2-6.jpg" alt="">',
+        '<img src="./img/work_2/2-7.jpg" alt="">'
+        ]
+
+};
+const ilmin = {
+    title: "Ilmin Museum",
+    type: 'type: web site',
+    text: '일민미술관의 웹페이지 리뉴얼 작업입니다. <br>컨셉 기획과 웹페이지 디자인, 구현까지의 작업을 진행하였습니다. <br> 일민 미술관의 초성을 심볼로 이용하며 현재 전시페이지와 미술관 소개 페이지를 나눠 구성하였습니다.',
+    tool: "tool : Visual Studio Code / Adobe  Photoshop",
+    date: "2020.05.16",
+    img:[
+        '<img src="./img/work_3/3-1.jpg" alt="">',
+        '<img src="./img/work_3/3-2.jpg" alt="">',
+        '<img src="./img/work_3/3-3.jpg" alt="">',
+    ]
+
+};
+const ff = {
+    title: "feel free",
+    type: 'type: video',
+    text: '가수 LANA DEL REY의 음악 RIDE 의 프로모션 비디오입니다. <br> 음악이 가지는 자유로운 분위기와 해방감을 주제로 작업하였으며, <br> 일러스트를 한 장 한장 이어서 제작했습니다.',
+    tool: "tool: Adobe Illustrator / Adobe After Effects",
+    date: "2019.10.14",
+    img:[
+        '<img src="./img/work_4/4-1.jpg" alt="">',
+        '<img src="./img/work_4/4-2.jpg" alt="">',
+        '<img src="./img/work_4/4-3.jpg" alt="">',
+        '<img src="./img/work_4/4-4.jpg" alt="">',
+        ]
+
+};
+const no = {
+    title: "Newtro Outfitters",
+    type: 'type: video',
+    text: '브랜드 Urban Outfitters 의 가상 기간 한정 프로모션 <br> "Newtro Outfitters"를 홍보하기 위한 온라인 브로셔 제작 작업입니다. <br> 한정판 LP를 소개하는 컨텐츠로 구성되었으며, <br> Newtro한 컨셉에 맞는 장난스러운 그래픽으로 젊은 느낌을 강조했습니다.', 
+    tool: "tool : Adobe Illustrator / Adobe  Photoshop",
+    date: "2019.09.11",
+    img:[
+        '<img src="./img/work_5/5-2.jpg" alt="">',
+    ]
+
+};
+
+
+const Myworks =[
+    nowadays,
+    remain,
+    werk,
+    ilmin,
+    ff,
+    no,
+];
